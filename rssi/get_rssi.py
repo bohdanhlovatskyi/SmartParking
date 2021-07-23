1be05e1686e0c6dfe022b3fa24cf2295c23348f6
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2021-07-23 09:48:51 +0300

    minor rssi analysis

--- rssi/get_rssi.py
+++ rssi/get_rssi.py
@@ -3,19 +3,17 @@ from bleak import BleakScanner
 from time import gmtime, strftime
 
 DETECTED_DEVICES = dict()
-MEASURED_POWER = -42
-RSSI_COEFFICIENT = 4 # from 2 to 4
+FILE_TO_SAVE = "two_devices_one_meter.csv"
 
 def detection_callback(device, _):
     if device.name is not None and device.name.startswith("SmartParking"):
         # print(device.name, "RSSI:", device.rssi)
         cur_time = strftime("%H:%M:%S", gmtime())
         DETECTED_DEVICES[cur_time] = (device.name, device.rssi)
-        with open("test.csv", "a") as file:
+        with open(FILE_TO_SAVE, "a") as file:
             file.write(f"{cur_time};{device.name};{device.rssi}\n")
 
-def get_distance(rssi: int):
-    "TODO"
+
 
 async def run():
     scanner = BleakScanner()
@@ -30,7 +28,9 @@ async def run():
 
 
 if __name__ == "__main__":
+    with open(FILE_TO_SAVE, "a") as file:
+        file.write("TIME;DEVICE;RSSI\n")
     loop = asyncio.get_event_loop()
-    for _ in range(50):
-        print("...") 
+    for i in range(50):
+        print(f"...{i}") 
         loop.run_until_complete(run())
